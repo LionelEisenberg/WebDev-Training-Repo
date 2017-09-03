54e6563765cd855d7681af8753186e382b17c72a
DESKTOP-CIRJV85\lione <lionel.eisenberg@gmail.com>
2017-09-03 18:24:27 -0400

    added ejs files for new routes in ejsDemo

--- Backend/MoreExpress/ejsDemo/app.js
+++ Backend/MoreExpress/ejsDemo/app.js
@@ -10,4 +10,13 @@ app.get("/filw/:thing", function(req, res) {
     res.render("love.ejs", {thingVar: thing});
 })
 
+app.get("/posts", function(req,res) {
+    var posts = [
+        {title: "post 1", author: "suzy"},
+        {title: "post 2", author: "suzay"},
+        {title: "post 3", author: "suzzay"}
+    ]
+    res.render("posts.ejs", {posts: posts});
+})
+
 app.listen(3000);
