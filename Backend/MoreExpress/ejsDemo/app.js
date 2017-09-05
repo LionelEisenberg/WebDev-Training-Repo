7879fb2ad4a33c10e3652128633dd8eb73ab4901
DESKTOP-CIRJV85\lione <lionel.eisenberg@gmail.com>
2017-09-04 22:01:32 -0400

    up to date now to do part 1 and 2 of posts

--- Backend/MoreExpress/ejsDemo/app.js
+++ Backend/MoreExpress/ejsDemo/app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
  });
 
 app.get("/filw/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 })
 
 app.get("/posts", function(req,res) {
@@ -16,7 +19,7 @@ app.get("/posts", function(req,res) {
         {title: "post 2", author: "suzay"},
         {title: "post 3", author: "suzzay"}
     ]
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 })
 
 app.listen(3000);
